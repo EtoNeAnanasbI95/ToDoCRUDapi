60d581325dc7038ee06eb5b0d5c7be470e158968
Dmitrii Kirilov <144320453+EtoNeAnanasbI95@users.noreply.github.com>
2024-12-18 10:40:58 +0300

    fix: fix bad interface

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -15,7 +15,7 @@ type Users interface {
 	Create(user *models.User) (int, error)
 	Get(id int) (models.User, error)
 	GetAll() ([]models.User, error)
-	Update(id int, user *models.UserInput) error
+	Update(id int, user *models.User) error
 	Delete(id int) error
 }
 
