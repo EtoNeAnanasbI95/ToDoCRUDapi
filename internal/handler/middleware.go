816ecb985f3892bae6c4bd431a484b4d6487ff4a
Dmitrii Kirilov <144320453+EtoNeAnanasbI95@users.noreply.github.com>
2025-01-05 23:02:48 +0300

    feat: add tasks handler

--- internal/handler/middleware.go
+++ internal/handler/middleware.go
@@ -1,1 +1,29 @@
 package handler
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
+
+func (h *Handler) CheckUserId(c *gin.Context) {
+	header := c.GetHeader("Authorization")
+	if header == "" {
+		h.log.Error("Authorization header is empty")
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"msg": "Authorization header is empty"})
+		return
+	}
+	id, err := strconv.Atoi(header)
+	if err != nil {
+		h.log.Error("Authorization header is invalid")
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"msg": err.Error()})
+		return
+	}
+	userId, err := h.services.Users.Get(id)
+	if err != nil {
+		h.log.Error(err.Error())
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"msg": err.Error()})
+		return
+	}
+	c.Set("uid", userId)
+}
--- internal/handler/tasks.go
+++ internal/handler/tasks.go
@@ -1,1 +1,23 @@
 package handler
+
+import "github.com/gin-gonic/gin"
+
+func (h *Handler) GetAllTasks(c *gin.Context) {
+
+}
+
+func (h *Handler) GetTask(c *gin.Context) {
+
+}
+
+func (h *Handler) UpdateTask(c *gin.Context) {
+
+}
+
+func (h *Handler) DeleteTask(c *gin.Context) {
+
+}
+
+func (h *Handler) CreateTask(c *gin.Context) {
+
+}
